dc7b8d5fe886b05237e724618001d140608f6f6a
Terence Wong <terencewong.work@gmail.com>
2020-11-07 18:02:03 -0500

    added flask as a requirement

--- src/index.py
+++ src/index.py
@@ -1,19 +1,35 @@
 from flask import Flask, request
+import requests
+from config import Config
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    print('args:', request.args)  # display text in console
-    print('form:', request.form)
-    print('data:', request.data)
-    print('json:', request.json)
-    print('files:', request.files)
-    #print("request:", request)
-    
 
-    print(request.args['code'])
-    return request.args.get('data', 'none')  # send text to web browser
+    print(request)
+    print(request.args)
+    #code = request.args['code']
+    #headers = {"Authorization": (Config['token'])}
+    #payload = {
+        #"client_id": Config['client_id'],
+        #"client_secret": Config['client_secret'],
+        #"grant_type": "authorization_code",
+        #"code": code,
+        #"redirect_uri": 
+    #}
+
+    #userJSON = requests.post(f"https://discord.com/api/oauth2/token{code}", headers=headers, data=)
+    #print ("hello")
+    #print (userJSON)
+    #print (userJSON.reason)
+    #print(userJSON.text.encode('utf8'))
+    return
+    # return userJSON.text  # send text to web browser
+
+@app.route("/auth", methods = ["get"])
+def auth():
+    return request.args['code']
 
 if __name__ == '__main__':
     app.run(port=80, debug=True)
