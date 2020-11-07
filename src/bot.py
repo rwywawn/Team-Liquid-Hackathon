2f184dc63fdd8c1ebe11a74fdc2c423d130bedba
luyaojchen <55671000+luyaojchen@users.noreply.github.com>
2020-11-07 13:53:44 -0500

    terences order

---
+++ src/bot.py
@@ -0,0 +1,19 @@
+import discord
+import os
+from discord.ext import commands
+from config import Config
+client = discord.Client()
+
+client = commands.Bot(command_prefix = '!')
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+#will load all cogs
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}') #take off last 3 characters since the .py part isn't needed
+
+#for our bot authentication
+client.run(Config['token'])
